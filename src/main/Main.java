bb66befdffa751d1b3951edb5c2068afed834456
Stefanie-K <71380307+Stefanie-K@users.noreply.github.com>
2020-10-13 12:13:27 +0200

    EDIT overview.png

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import assignments.SoftCon2020_Assignment_2.testclass;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        testclass.print_battlefield();
     }
 }
