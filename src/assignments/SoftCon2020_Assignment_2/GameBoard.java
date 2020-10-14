221284042da59ff0abca7a3a3f0b5b1138287aaf
Loris De Luca <loris.deluca@uzh.ch>
2020-10-14 13:18:45 +0200

    EDIT set some stuff private, still works

--- src/assignments/SoftCon2020_Assignment_2/GameBoard.java
+++ src/assignments/SoftCon2020_Assignment_2/GameBoard.java
@@ -12,13 +12,13 @@ public class GameBoard {
         }
     }
 
-    void display() {
+    public void display() {
         System.out.println("   |[A][B][C][D][E][F][G][H][I][L]\n---|------------------------------");
         print(board);
         //System.out.println(Arrays.deepToString(board));
     }
 
-    public static void print(Ship[][] board) {
+    private static void print(Ship[][] board) {
         for (int i = 0; i < board.length; i++) {
             System.out.print("["+i+"]|");
             for (int j = 0; j < board[i].length; j++) {
